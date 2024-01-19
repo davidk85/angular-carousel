1b517e6b5ddda17a214e5d47151549557eb00cf3
David Kahl <david.kahl@go-ahead.de>
2024-01-19 07:50:47 +0100

    install

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IvyCarouselModule } from 'projects/angular-responsive-carousel/src/public-api';
 
-import { IvyCarouselModule } from 'angular-responsive-carousel';
 
 @NgModule({
   declarations: [
